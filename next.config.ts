13d69412dfaf6f8a5bed631abc1ebc1b5c4378cf
Maurolima14555 <mauropastrana90@gmail.com>
2026-05-29 00:13:51 +0100

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
